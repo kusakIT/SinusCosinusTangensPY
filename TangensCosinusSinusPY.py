1353fec06368f922f9e5e7d8c15f04d534418c84
kusakIT <137216778+kusakIT@users.noreply.github.com>
2023-06-29 00:51:25 +0200

    Create TangensCosinusSinusPY.py

---
+++ TangensCosinusSinusPY.py
@@ -0,0 +1,11 @@
+import math
+
+kat_w_radianach = math.radians(45)
+
+sinus = math.sin(kat_w_radianach)
+cosinus = math.cos(kat_w_radianach)
+tangens = math.tan(kat_w_radianach)
+
+print("Sinus:", sinus)
+print("Cosinus:", cosinus)
+print("Tangens:", tangens)
